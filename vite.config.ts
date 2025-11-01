225653b0e3db074cee6832b103c3279a8adda977
Sayantan Khan <iamstk14@gmail.com>
2025-11-01 11:14:28 +0530

    adding base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/dev-tools",
+  base: mode === "production" ? "/tools/" : "/",
   server: {
     host: "::",
     port: 8080,
